b2b44c86219ab3772de8c671fd084469667b8da6
Mohamed Shaheed <64309668+shaheedams@users.noreply.github.com>
2025-04-15 11:40:56 +0530

    Added all the sections and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
-// Initialize Lenis
-const lenis = new Lenis({
-    autoRaf: true,
+
+$(document).ready(function () {
+    // Initialize Lenis
+    const lenis = new Lenis({
+        autoRaf: true,
+    });
+
+    // Misc active trigger
+    $('.miscNavBtns .btn').on('click', function (e) {
+        let id = $(e.target).attr('data-id');
+        $(`.misc_container, .miscNavBtns .btn`).removeClass('active');
+        $(`#${id}`).addClass('active');
+        $(e.target).addClass('active');
+    });
+
+    // Nav links active trigger
+    const sections = document.querySelectorAll(".sec");
+    window.addEventListener("scroll", navHighlighter);
+    function navHighlighter() {
+        let scrollY = window.pageYOffset;
+        sections.forEach(current => {
+            const sectionHeight = current.offsetHeight;
+            const sectionTop = current.offsetTop - 50;
+            sectionId = current.getAttribute("id");
+            if (
+                scrollY > sectionTop &&
+                scrollY <= sectionTop + sectionHeight && sectionId !== 'slogan' && sectionId !== 'skills' && sectionId !== 'recognition'
+            ) {
+                $('.nav a').removeClass('active');
+                document.querySelector(".nav a[href*=" + sectionId + "]").classList.add("active");
+            }
+        });
+    }
+
 });
+
 
